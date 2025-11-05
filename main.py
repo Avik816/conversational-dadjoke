000b0597e3a1ec1367809cdc17c71379b2fdc20e
Avik816 <manikchakrabarti745@gmail.com>
2025-11-05 22:28:21 +0530

    refactor(main): import preprocessing function from src module

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from src import *
 
 
+print('\n\nDownloading the dataset...\n')
 download_dataset()
+
+print('\n\nMerging the dataset...\n')
 merge_dataset()
+
+print('\n\nPreprocessing the dataset...\n')
+preprocess_dataset()
