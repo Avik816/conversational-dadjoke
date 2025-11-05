3a9aa7650c53fd0cf2fab276138b3e91adf9531c
Avik816 <manikchakrabarti745@gmail.com>
2025-11-05 22:22:14 +0530

    docs(data_utils): add explanatory comments

--- src/data_utils/downloader.py
+++ src/data_utils/downloader.py
@@ -1,3 +1,7 @@
+# This scripts download the dataset from Hugging Face Datasets.
+# It originally downloads in arrow format and splitted as train and tests.
+# This scripts converts the dataset from the said format to .csv.
+
 from datasets import load_dataset
 from pathlib import Path
 
@@ -5,9 +9,9 @@ from pathlib import Path
 def download_dataset():
     dataset = load_dataset('shuttie/dadjokes')
 
-    Path('src/data/raw').mkdir(parents=True, exist_ok=True)
+    Path('data/raw').mkdir(parents=True, exist_ok=True)
 
-    dataset['train'].to_csv('src/data/raw/train_raw.csv')
-    dataset['test'].to_csv('src/data/raw/test_raw.csv')
+    dataset['train'].to_csv('data/raw/train_raw.csv')
+    dataset['test'].to_csv('data/raw/test_raw.csv')
 
     return 'Raw Datasets downloaded !\n'
--- src/data_utils/merger.py
+++ src/data_utils/merger.py
@@ -1,17 +1,20 @@
+# This script converts the two train and test dataset into one single parent dataset.
+
+
 import polars
 from pathlib import Path
 
 
 def merge_dataset():
-    train = polars.read_csv('src/data/raw/train_raw.csv')
-    test = polars.read_csv('src/data/raw/test_raw.csv')
+    train = polars.read_csv('data/raw/train_raw.csv')
+    test = polars.read_csv('data/raw/test_raw.csv')
 
     dataset = train.vstack(test)
 
     print(dataset.head())
 
-    Path('src/data/merged').mkdir(parents=True, exist_ok=True)
+    Path('data/merged').mkdir(parents=True, exist_ok=True)
 
-    dataset.write_csv('src/data/merged/dad_jokes.csv')
+    dataset.write_csv('data/merged/dad_jokes.csv')
 
     return 'Full Dataset saved !\n'
