38499f40ddf1bccb3e876f72bb328668007c54ca
Avik816 <manikchakrabarti745@gmail.com>
2025-11-07 23:59:54 +0530

    chore(utils): add directory creation module

---
+++ src/utils/setup_dirs.py
@@ -0,0 +1,18 @@
+# This script creates the necessary directories for this project
+
+from config.paths import *
+from pathlib import Path
+import os
+
+
+def create_dirs():
+    if not os.path.exists(RAW_SET_DIR):
+        Path('data/raw').mkdir(parents=True, exist_ok=True)
+    if not os.path.exists(MERGED_SET_DIR):
+        Path('data/merged').mkdir(parents=True, exist_ok=True)
+    if not os.path.exists(FINAL_SET_DIR):
+        Path('data/cleaned').mkdir(parents=True, exist_ok=True)
+    if not os.path.exists(MODEL_DIR):
+        Path('models/embedder/')
+
+    return 'Necessary Directories created.\n'
