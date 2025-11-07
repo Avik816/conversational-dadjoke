fcc19f6b742b154229bb71ae3a3ce7fded97d6e7
Avik816 <manikchakrabarti745@gmail.com>
2025-11-08 00:01:51 +0530

    chore(main): update main to create dirs automatically

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from src import *
 
 
-print('\n\nDownloading the dataset...\n')
+print('Creating necessary Directories...')
+create_dirs()
+"""print('\n\nDownloading the dataset...\n')
 download_dataset()
 
 print('\n\nMerging the dataset...\n')
 merge_dataset()
 
 print('\n\nPreprocessing the dataset...\n')
-preprocess_dataset()
+preprocess_dataset()"""
