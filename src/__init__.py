5918d6e63e728c0f44169650fad84159d9a5e185
Avik816 <manikchakrabarti745@gmail.com>
2025-11-09 23:10:38 +0530

    feat(init): update init to manage embedder module

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,8 @@
+# This module holds all the imports of modules for main to execute
+
+
 from utils.setup_dirs import create_dirs
 from data_utils.downloader import download_dataset
 from data_utils.merger import merge_dataset
 from data_utils.preprocessor import preprocess_dataset
+from embedding.embedder import generate_embeddings
