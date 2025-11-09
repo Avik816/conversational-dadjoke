500e0a3921b7c68b548c11834d21404124426e26
Avik816 <manikchakrabarti745@gmail.com>
2025-11-09 23:13:00 +0530

    feat(main): update main to integrate embedder function via package init

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+# This is the main script that runs the entire system.
+
+
 from src import *
 
 
 print('Creating necessary Directories...')
 create_dirs()
+
 """print('\n\nDownloading the dataset...\n')
 download_dataset()
 
-print('\n\nMerging the dataset...\n')
+print('\n\nMerging the dataset...')
 merge_dataset()
 
-print('\n\nPreprocessing the dataset...\n')
+print('\n\nPreprocessing the dataset...')
 preprocess_dataset()"""
+
+print('Embedding the jokes...')
+generate_embeddings()
