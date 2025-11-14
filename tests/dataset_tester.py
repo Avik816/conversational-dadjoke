3a34b775ef0bf7d253dc2219faa64df99ce415b8
Avik816 <manikchakrabarti745@gmail.com>
2025-11-14 23:55:12 +0530

    test: add test file for dataset schema

---
+++ tests/dataset_tester.py
@@ -0,0 +1,17 @@
+import polars, faiss
+
+
+dataset = polars.read_parquet('db/vector_embedds/dadjoke_embedded.parquet')
+# print(dataset.head(1))
+
+del dataset
+
+dataset = polars.read_parquet('db/vector_index/faiss_id_map.parquet')
+# print(dataset.head())
+
+# Load the index
+index = faiss.read_index("db/vector_index/dadjoke_faiss.index")
+print(index)
+
+# Check how many vectors are in it
+print("Number of vectors:", index.ntotal)
